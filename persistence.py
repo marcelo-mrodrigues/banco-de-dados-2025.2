0516ef84a8b3f95a8718bef9285b410ca694328e
Canbomm <canbomim@hotmail.com>
2025-12-03 17:02:46 -0300

    crud usuario

--- persistence.py
+++ persistence.py
@@ -1,12 +1,12 @@
 import mysql.connector
 
-class ParqueDB:
-  def __init__(self):
+class Database:
+  def __init__(self,dbname,dbuser="admin",dbpasswd="admin"):
     self.db = mysql.connector.connect(
       host="localhost",
-      user="admin",
-      passwd="admin",
-      database="gestao_parques"
+      user=dbuser,
+      passwd=dbpasswd,
+      database=dbname
     )
     self.cursor = self.db.cursor()
 
@@ -18,7 +18,7 @@ class ParqueDB:
     self.cursor.execute(query)
     self.db.commit()
 
-  def createTable(self,tabela,dados):
+  def insertTable(self,tabela,dados):
     # -- Dados é um dicionário dos valores desejados
     # Chave é o nome da coluna
     # Valor é o valor a ser inserido
@@ -32,7 +32,7 @@ class ParqueDB:
       placeholders.append("%s")
     
     query = f"insert into {tabela} ({", ".join(columns)}) values ({", ".join(placeholders)})"
-    self.execute(query,values)
+    return self.executeQuery(query,values)
 
   def readTable(self, tabela, colunas=["*"], filtros=None):
     # -- Colunas é uma lista de strings com as colunas desejadas
@@ -79,7 +79,7 @@ class ParqueDB:
       identifier_placeholder.append(key + " = %s")
     
     query = f"update {tabela} set {", ".join(value_placeholder)} where {" AND ".join(identifier_placeholder)}"
-    self.execute(query,values+identifiers)
+    return self.executeQuery(query,values+identifiers)
 
   def deleteTable(self,tabela,identificadores):
     # -- Identificador deverá ser um dicionário
@@ -98,7 +98,7 @@ class ParqueDB:
       placeholders.append(key + " = %s")
     
     query = f"delete from {tabela} where {" AND ".join(placeholders)}"
-    self.execute(query,identifiers)
+    return self.executeQuery(query,identifiers)
 
   def openBlob(self,path):
     # Para salvar arquivos de outros tipos como pdf e/ou jpg
@@ -108,18 +108,79 @@ class ParqueDB:
     
   def reservar_procedure(self, id_usuario, id_equipamento, inicio, fim):
     try: #nova_rezerva
-        args = (id_usuario, id_equipamento, inicio, fim)
+      args = (id_usuario, id_equipamento, inicio, fim)
         
-        self.cursor.callproc('sp_nova_reserva', args)
-        self.db.commit()
-        print("Sucesso na reserva")
-        return True
-        
+      self.cursor.callproc('sp_nova_reserva', args)
+      self.db.commit()
+      print("Sucesso na reserva")
+      return True
+      
     except mysql.connector.Error as err:
-        print(f"Erro ao reservar: {err}")
-        return False
-
+      print(f"Erro ao reservar: {err}")
+      return False
 
   def __exit__(self):
     self.cursor.close()
     self.db.close()
+
+class ParqueBD:
+  def __init__(self):
+    self.db = Database("gestao_parques")
+
+  def createUser(self,nome,cpf,email):
+    return self.db.insertTable("Usuario",{"nome_completo":nome,"cpf":cpf,"email":email})
+  
+  def readUser(self,userID=None,name=None,cpf=None,email=None):
+    if (not userID) and ((not name) or (not cpf) or (not email)):
+      return "Identifique o usuario pelo ID ou atributos"
+
+    if name and cpf and email:
+      return self.db.readTable("Usuario",filtros={"nome_completo":name,"cpf":cpf,"email":email})
+    return self.db.readTable("Usuario",filtros={"id_usuario":userID})
+  
+  def updtUser(self,userID=None,name=None,cpf=None,email=None,newname=None,newcpf=None,newemail=None):
+    if (not userID) and ((not name) or (not cpf) or (not email)):
+      return "Identifique o usuario pelo ID ou atributos"
+    
+    # Prioriza identificar por nome cpf e email para
+    # permitir uma insercao mais simples passando ID aleatorio
+    if name and cpf and email:
+      identificadores = {"nome_completo":name,"cpf":cpf,"email":email}
+    else:
+      identificadores = {"id_usuario":userID}
+
+    invalid = 0 # se os tres campos forem vazios nao ha o que atualizar
+    novos_valores = {}
+    if newname:
+      novos_valores["nome_completo"] = newname
+      invalid += 1
+    if newcpf:
+      novos_valores["cpf"] = newcpf
+      invalid += 1
+    if newemail:
+      novos_valores["email"] = newemail
+      invalid += 1
+    if invalid < 1:
+      return "Altere pelo menos um valor"
+    
+    return self.db.updateTable("Usuario",novos_valores,identificadores)
+  
+  def deleteUser(self,userID=None,name=None,cpf=None,email=None):
+    if (not userID) and ((not name) or (not cpf) or (not email)):
+      return "Identifique o usuario pelo ID ou atributos"
+
+    if name and cpf and email:
+      return self.db.deleteTable("Usuario",{"nome_completo":name,"cpf":cpf,"email":email})
+    return self.db.deleteTable("Usuario",{"id_usuario":userID})
+
+mybd = ParqueBD()
+# print(mybd.createUser("Claudio","00000000001","claudio@fake.com"))
+# print(mybd.readUser("1"))
+# print(mybd.readUser("-1","Claudio","00000000001","claudio2@fake.com"))
+# print(mybd.updtUser(userID="1",newemail="claudio3@fake.com"))
+# print(mybd.updtUser("-1","Claudio","00000000001","claudio3@fake.com",newemail="claudio2@fake.com"))
+# print(mybd.deleteUser("1"))
+# print(mybd.createUser("Irmao do Claudio","00000000002","claudiobrother@fake.com"))
+# print(mybd.readUser("1"))
+# print(mybd.readUser("2"))
+# print(mybd.deleteUser("-1","Irmao do Claudio","00000000002","claudiobrother@fake.com"))
